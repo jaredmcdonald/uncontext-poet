626ed01f7bf106ae84311f987121e60c77b9e515
Jared McDonald <jared.r.mcdonald@gmail.com>
2015-08-30 12:16:42 -0400

    live algorithm editor

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,22 @@
 (function (SOURCE_TEXT_SPLIT) {
-  var el = document.getElementById('container');
-  var userScrolled = false;
+  const ALG_TOKEN_REGEX = /^[abcefms+-/*\(\)1234567890]$/
+  const SPACE_REGEX = /\s/;
+
+  const container = document.getElementById('container');
+  const controls = document.getElementById('editor');
+  const sample = document.getElementById('sample');
+
+  const ws = io();
+
+  let userScrolled = false;
+  let algorithm = getAlgorithm();
 
   function getWord (dec) {
     return SOURCE_TEXT_SPLIT[Math.floor(SOURCE_TEXT_SPLIT.length * dec)];
   }
 
   function appendText (w) {
-    el.innerText += ' ' + w;
+    container.innerText += ' ' + w;
   }
 
   function onScroll () {
@@ -19,26 +28,76 @@
     window.scrollTo(0, document.body.scrollHeight);
   }
 
-  var ws = io();
+  function validateAlgorithm (algString) {
+    // validate individual tokens
+    // (we get away with this because each token is one character)
+    const tokensValid = algString.split('')
+                                 .filter((c) => !SPACE_REGEX.test(c))
+                                 .every((item) => ALG_TOKEN_REGEX.test(item));
 
+    if (!tokensValid) return false;
+
+     // test w/ real values and make sure it doesn't throw
+     const [a, b, c, f, g] = [5, 1, 0, 175, 199];
+     const m = Math.max(a, b, c, f, g);
+     const s = a + b + c + f + g;
+     let valueTestPasses = true;
+     try {
+       eval(algString);
+     } catch (e) {
+       valueTestPasses = false;
+     };
+     return valueTestPasses;
+  }
+
+  function getAlgorithm () {
+    const alg = editor.value;
+    const isValid = validateAlgorithm(alg);
+    if (!isValid) return false;
+
+    return alg;
+  }
+
+  // return a number between 0 and 1
+  function normalizeResult (result) {
+    const abs = Math.abs(result);
+    return abs > 1 ? 1 / abs : abs;
+  }
+
+  function onAlgChange () {
+    const alg = getAlgorithm();
+    if (alg) {
+      algorithm = alg;
+    } else {
+      console.error('invalid algorithm');
+    }
+  }
+
+  function onMessage (message) {
+    const {a, b, c, e: {f, g}} = JSON.parse(message);
+    const m = Math.max(a, b, c, f, g); // max
+    const s = a + b + c + f + g; // sum
+    sample.innerText =
+      `${JSON.stringify({a, b, c, f, g})}\nm: ${m}, s: ${s}`;
+
+    // eval time :/
+    // should be reasonably safe since `algorithm` is scrubbed pre-save
+    const result = eval(algorithm);
+    const normalized = normalizeResult(result);
+    const word = getWord(normalized);
+    appendText(` ${word || '\n'}`);
+    // arbitrary line break logic
+    if (Math.abs(0.5 - normalized) <= 0.01) {
+      appendText('\n');
+    }
+
+    if (!userScrolled) {
+      updateScroll();
+    }
+  }
+
+  controls.addEventListener('change', onAlgChange);
   window.addEventListener('scroll', onScroll);
-
-  ws.on('message', function (message) {
-    var data = JSON.parse(message);
-    var nums = [data.a, data.b, data.e.f, data.e.g];
-    var messageMax = Math.max.apply(null, nums);
-
-    nums.map(function (r) {
-      return getWord(r / messageMax);
-    }).forEach(function (w) {
-      setTimeout(function () {
-        appendText(w ? ' ' + w : '\n');
-        if (!userScrolled) {
-          updateScroll();
-        }
-      }, (1 / data.c) * 1000);
-    });
-
-  });
+  ws.on('message', onMessage);
 
 })(window.PARSED_DATA);
